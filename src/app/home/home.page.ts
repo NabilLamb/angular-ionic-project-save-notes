056c4fe1d8222ccb5c3d9c940df52faa866e09ce
Nabil Lambattan <lambattannabil2000@gmail.com>
2024-01-31 23:51:51 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
+import { AlertController, ModalController } from '@ionic/angular';
+import { DataService } from './../services/data.service';
 import { Component } from '@angular/core';
+import { ModalPage } from '../modal/modal.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  notes: any = [];
+
+
+  constructor(private dataService: DataService, private alertCtrl: AlertController, private modalCtrl: ModalController) {
+    this.dataService.getNotes().subscribe(
+      res => {
+        console.log(res);
+        this.notes = res;
+      }
+    )
+  }
+
+  async openNote(note: any) {
+    const modal = await this.modalCtrl.create({
+      component: ModalPage,
+      componentProps: { id: note.id },
+      breakpoints: [0, 0.5, 0.8],
+      initialBreakpoint: 0.5
+    });
+
+    modal.present();
+  }
+
+  async addNote() {
+    const alert = await this.alertCtrl.create({
+      header: 'Add Note',
+      inputs: [
+        {
+          name: 'Title',
+          placeholder: 'Type Title',
+          type: 'text'
+        },
+        {
+          name: 'Text',
+          placeholder: 'Type Text',
+          type: 'textarea'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }, {
+          text: 'Add',
+          handler: res => {
+            this.dataService.addNote({ text: res.text, title: res.title });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
 
 }
